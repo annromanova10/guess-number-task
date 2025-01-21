6b4c966c57e65d6a4ec12a78e682c50e06d3bbe5
Anna <agromanova10@gmail.com>
2025-01-21 14:10:37 +0300

    change min_n, max_n. add count_attempt

--- game_my_version.py
+++ game_my_version.py
@@ -1,4 +1,6 @@
 # функция для оценки
+import numpy as np
+
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
 
@@ -30,17 +32,17 @@ def game_core_v3(number: int = 1) -> int:
     """
     # Ваш код начинается здесь
     count = 0
-    min_n, max_n = 1, 100 #объявляем границы как минимум и максимум
+    min_n, max_n = 0, 101 #объявляем границы как минимум и максимум
     predict_number = np.random.randint(1,100) # предполагаемое число
 
     while predict_number != number: #цикл пока загаданное не равно нашему
       count += 1 # увеличиваем счетчик
       if predict_number > number:
         max_n = predict_number # меняем верхнюю границу
-        predict_number = max_n - (max_n - min_n)//2
+        predict_number = (max_n + min_n)//2
       elif predict_number < number:
         min_n = predict_number # меняем нижнюю границу
-        predict_number = min_n + (max_n + min_n)//2
+        predict_number = (max_n + min_n)//2
 
     # Ваш код заканчивается здесь
    
@@ -49,3 +51,26 @@ def game_core_v3(number: int = 1) -> int:
 # оценка качества
 print('Run benchmarking for game_core_v3: ', end='')
 score_game(game_core_v3)
+
+
+
+def count_attempt(loocking_number):
+    """ Функция для сбора статистики расчетов нашего когда.
+        Принимает функцию, выдает сообщение о максимальном, минимальном и среднем
+        количестве результатов выполнения нашего кода за 1000 запусков
+    """
+    
+    count_lst = []
+    np.random.seed(5)
+    random_array = np.random.randint(1, 101, size=(1000))
+    
+    for number in random_array:   
+        count_lst.append(loocking_number(number))
+
+
+    print(f'Среднее число попыток {int(np.mean(count_lst))}') # Ожидаемо 6
+    print(f'Максимальное количество попыток {max(count_lst)}') # Ожидаемо до 8 при целочисленном делении диапазона
+    print(f'Минимальное количество попыток {min(count_lst)}') # Ожидаемо 1
+    
+    
+count_attempt(game_core_v3)
